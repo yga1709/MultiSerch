c65e9378f067a9ae6a00857a11282c987b8b588e
a1709 <ygbox0324@gmail.com>
2019-01-15 13:40:01 +0900

    add : 検索機能を追加

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,48 @@
-getSerchEngines = engin => {
-  let engins = {};
-  engins["yahoo"] = "http://search.yahoo.co.jp/search?p=";
-  engins["google"] = "https://www.google.com/search?q=";
-  engins["amazon"] =
-    "https://www.amazon.co.jp/gp/search/ref=nb_sb_noss?__mk_ja_JP=カタカナ&url=search-alias%3Daps&field-keywords=";
-  engins["googleImage"] =
-    "https://www.google.co.jp/search?newwindow=1&site=&tbm=isch&source=hp&q=";
-  const getData = getCustomEngines(data => {}, engins);
-  console.log(getData);
-  return engins;
+var engins = {};
+engins["Yahoo"] = "http://search.yahoo.co.jp/search?p=";
+engins["Google"] = "https://www.google.com/search?q=";
+engins["Amazon"] =
+  "https://www.amazon.co.jp/gp/search/ref=nb_sb_noss?__mk_ja_JP=カタカナ&url=search-alias%3Daps&field-keywords=";
+engins["Google画像検索"] =
+  "https://www.google.co.jp/search?newwindow=1&site=&tbm=isch&source=hp&q=";
+engins["Bing"] = "https://www.bing.com/search?q=";
+engins["YouTube"] = "https://www.youtube.com/search?q=";
+engins["niconico"] = "https://www.nicovideo.com/";
+
+init = engin => {
+  const getData = getCustomEngines(data => {
+    let resultData = data.custom;
+    let viewEngins = document.getElementById("viewEngins");
+
+    for (let site in data.custom) {
+    } //ここでcustomサイトをenginsに登録する処理
+
+    //enginsのサイトをポップアップに表示するループ
+    for (let engin in engins) {
+      console.log(engins[engin]);
+      let html = `<input type="checkbox" name="engine" value="${engin}" />${engin} `;
+      viewEngins.insertAdjacentHTML("afterbegin", html);
+    }
+  });
 };
 
-getCustomEngines = (callback, engins) => {
+getCustomEngines = callback => {
   chrome.storage.sync.get("custom", callback);
 };
 
 document.getElementById("search").onclick = () => {
   const serchList = document.getElementsByName("engine");
-  const engines = getSerchEngines();
+  const serchWord = document.getElementById("keyword").value;
+  //チェックされた値を取得して検索
+  for (let i = 0; i < serchList.length; i++) {
+    if (serchList[i].checked) {
+      const selectEngine = serchList[i].value;
+      const margeURL = `${engins[selectEngine]}${serchWord}`;
+      chrome.tabs.create({ url: margeURL });
+    }
+  }
+};
+
+window.onload = () => {
+  init();
 };
