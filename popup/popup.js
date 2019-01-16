d4a33a0728ddbab85dd0a1c01f9a589bb3de2589
a1709 <ygbox0324@gmail.com>
2019-01-16 10:48:06 +0900

    add : Storageからデータを取得する機能

--- popup/popup.js
+++ popup/popup.js
@@ -7,23 +7,33 @@ engins["Google画像検索"] =
   "https://www.google.co.jp/search?newwindow=1&site=&tbm=isch&source=hp&q=";
 engins["Bing"] = "https://www.bing.com/search?q=";
 engins["YouTube"] = "https://www.youtube.com/search?q=";
-engins["niconico"] = "https://www.nicovideo.com/";
+engins["niconico"] = "https://www.nicovideo.jp/search/";
 
 init = engin => {
   const getData = getCustomEngines(data => {
     let resultData = data.custom;
-    let viewEngins = document.getElementById("viewEngins");
-
-    for (let site in data.custom) {
-    } //ここでcustomサイトをenginsに登録する処理
-
-    //enginsのサイトをポップアップに表示するループ
-    for (let engin in engins) {
-      console.log(engins[engin]);
-      let html = `<input type="checkbox" name="engine" value="${engin}" />${engin} `;
-      viewEngins.insertAdjacentHTML("afterbegin", html);
+    try {
+      resultData = JSON.parse(resultData);
+      for (let site in resultData) {
+        console.log(resultData[site].name);
+        engins[resultData[site].name] = resultData[site].url;
+        console.log(resultData[site].url);
+      }
+    } catch (e) {
+      console.log("localStorageにデータが存在しません。");
     }
+    //ここでcustomサイトをenginsに登録する処理
   });
+  viewEngins();
+};
+
+viewEngins = () => {
+  let viewEngins = document.getElementById("viewEngins");
+  let html;
+  for (let engin in engins) {
+    html = `<span id="${engin}"><input type="checkbox" name="engine" value="${engin}"/>${engin}</span>`;
+    viewEngins.insertAdjacentHTML("afterbegin", html);
+  }
 };
 
 getCustomEngines = callback => {
@@ -31,16 +41,33 @@ getCustomEngines = callback => {
 };
 
 document.getElementById("search").onclick = () => {
-  const serchList = document.getElementsByName("engine");
   const serchWord = document.getElementById("keyword").value;
-  //チェックされた値を取得して検索
-  for (let i = 0; i < serchList.length; i++) {
-    if (serchList[i].checked) {
-      const selectEngine = serchList[i].value;
-      const margeURL = `${engins[selectEngine]}${serchWord}`;
-      chrome.tabs.create({ url: margeURL });
+  if (!serchWord) return 0;
+  const serchList = getChecked();
+  for (let engin of serchList) {
+    const margeURL = `${engins[engin]}${serchWord}`;
+    chrome.tabs.create({ url: margeURL });
+  }
+};
+
+document.getElementById("delete").onclick = () => {
+  const deleteList = getChecked();
+  for (let del of deleteList) {
+    delete engins[del];
+    let elem = document.getElementById(del);
+    elem.parentElement.removeChild(elem);
+  }
+};
+
+getChecked = () => {
+  const checkList = document.getElementsByName("engine");
+  const checkedArray = [];
+  for (let i = 0; i < checkList.length; i++) {
+    if (checkList[i].checked) {
+      checkedArray.push(checkList[i].value);
     }
   }
+  return checkedArray;
 };
 
 window.onload = () => {
